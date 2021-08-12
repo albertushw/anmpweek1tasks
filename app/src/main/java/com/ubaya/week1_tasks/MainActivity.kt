ae19a54fc6b26234e28858d7c83e5092658d50bf
Motzz <s160419007@student.ubaya.ac.id>
2021-08-12 21:46:54 +0700

    testalbet

--- app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
+++ app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     val number2 = 4
     val testAngka=21
     val testnama="haii"
+    val albet=221
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
