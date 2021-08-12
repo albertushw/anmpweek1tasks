8b2a0627f75416a0ab3c700b7a5256266939a9b5
LogionStevandie <s160419013@student.ubaya.ac.id>
2021-08-12 21:34:05 +0700

    tanggal added

--- app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
+++ app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     val nama = "bagus"
     val jumlah = 29
+    val tanggal = "agustus 12 2021"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
