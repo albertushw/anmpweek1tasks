90168d8e829cdc3866ca0ec080f4604a564a3491
LogionStevandie <s160419013@student.ubaya.ac.id>
2021-08-12 21:34:58 +0700

    toast2 added

--- app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
+++ app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
@@ -6,10 +6,16 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     val nama = "bagus"
+
+    val number1 = 2
+    val number2 = 4
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Toast.makeText(this, "Halo $nama", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "yooooo", Toast.LENGTH_SHORT).show()
+        var hasil = number1*number2
+        Toast.makeText(this, "Hasil perhitungan adalah $hasil", Toast.LENGTH_LONG).show()
     }
 }
