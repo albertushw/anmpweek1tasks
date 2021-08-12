c55dd5dcdacc2d0468538a20228ccd1ecaadb9ee
LogionStevandie <s160419013@student.ubaya.ac.id>
2021-08-12 21:21:04 +0700

    nama added

--- app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
+++ app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
@@ -5,10 +5,11 @@ import android.os.Bundle
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val nama = "bagus"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "Halo", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Halo $nama", Toast.LENGTH_SHORT).show()
     }
 }
