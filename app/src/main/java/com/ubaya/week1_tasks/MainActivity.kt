19c3faa988e70001a16219af8e10222a4a9713c9
acer <s160419009@student.ubaya.ac.id>
2021-08-12 21:10:12 +0700

    init task week 1 group

---
+++ app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ubaya.week1_tasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Halo", Toast.LENGTH_SHORT).show()
+    }
+}
