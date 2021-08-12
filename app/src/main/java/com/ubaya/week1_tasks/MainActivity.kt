6be89928e797d69cd0dbaebcbc59afb468e23ea6
Motzz <s160419007@student.ubaya.ac.id>
2021-08-12 21:44:58 +0700

    testNama

--- app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
+++ app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     val nama = "bagus"
+    val testAngka=21
+    val testnama="haii"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
