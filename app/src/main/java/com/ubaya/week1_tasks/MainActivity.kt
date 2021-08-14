79a62430f7dee6260bf238f77b26d327aacbcc94
Motzz <s160419007@student.ubaya.ac.id>
2021-08-14 20:55:15 +0700

    nambah di count

--- app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
+++ app/src/main/java/com/ubaya/week1_tasks/MainActivity.kt
@@ -6,40 +6,15 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     val nama = "bagus"
-    val jumlah = 29
-    val tanggal1 = "agustus 12 2020"
-    val tanggal2 = "agustus 12 2021"
 
     val number1 = 2
     val number2 = 4
-    val testAngka=21
-    val testnama="haii"
-    val albet=221
-
-    val number3 = 2
-    val number4 = 4
-
-    var feat = "hello"
-    var ures = "myname"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "Halo $nama $jumlah", Toast.LENGTH_SHORT).show()
         Toast.makeText(this, "Halo $nama", Toast.LENGTH_SHORT).show()
-        Toast.makeText(this, "yooooo", Toast.LENGTH_SHORT).show()
         var hasil = number1*number2
         Toast.makeText(this, "Hasil perhitungan adalah $hasil", Toast.LENGTH_LONG).show()
-
-        while(feat!=ures){
-            ures = feat
-        }
-
-        if (hasil=20){
-            feat="test"
-        }
-        if (hasil=204){
-            feat="wau"
-        }
     }
 }
